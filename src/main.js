d3272864de52f4a4652631a416f91bb2c9625f3e
yinzhenwei <yinwhm12@163.com>
2017-07-08 14:45:01 +0800

    7.8 前端时间转换 格式

--- src/main.js
+++ src/main.js
@@ -46,6 +46,49 @@ export var router = new VueRouter({
   }]
 })
 
+Date.prototype.format = function (format) {
+  var date = {
+    "M+": this.getMonth() + 1,
+    "d+": this.getDate(),
+    "h+": this.getHours(),
+    "m+": this.getMinutes(),
+    "s+": this.getSeconds(),
+    "q+": Math.floor((this.getMonth() + 3) / 3),
+    "S+": this.getMilliseconds()
+  };
+  if (/(y+)/i.test(format)) {
+    format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (var k in date) {
+    if (new RegExp("(" + k + ")").test(format)) {
+      format = format.replace(RegExp.$1, RegExp.$1.length == 1
+        ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+    }
+  }
+  return format;
+}
+
+Vue.filter("stampToTime",(timestamp) => {
+  "use strict";
+  if (!timestamp){
+    return ""
+  }
+  let newDate = new Data()
+  newDate.setTime(timestamp * 1000)
+  return newDate.format('yyyy-MM-dd')
+})
+
+Vue.filter("stampToTimeFull", (timestamp) => {
+  "use strict";
+  if (!timestamp) {
+    return ""
+  }
+  let newDate = new Date()
+  newDate.setTime(timestamp * 1000)
+  return newDate.format('yyyy-MM-dd hh:mm:ss')
+})
+
+
 new Vue({
   el: '#app',
   router: router,
